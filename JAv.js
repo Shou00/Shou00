12c9663f99f19fa02028594ae575ac5d678e420b
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-26 01:51:17 +0800

    Update JAv.js

--- JAv.js
+++ JAv.js
@@ -1,51 +1,50 @@
-                     
-
 /*
- *
- *
-脚本功能：JAv破解VIP
+寰俊鍏紬鍙凤細ios榛戠鎶€
+瀹樻柟缃戠珯锛歴7aa.cn
 
-*******************************
+鍦圶:
+
 [rewrite_local]
-# > JAv解锁订阅
-^https:\/\/yiliang8888\.com\/api\/v1\/sessions.+ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/JAv.js
-[mitm] 
-hostname = yiliang8888.com
-*
-*
+
+#JAV瑙ｉ攣VIP浼氬憳
+^https:\/\/yugedu\.com\/api\/v1\/movies\/(.+)\/play url script-request-header javdba.js
+
+^https:\/\/yugedu\.com\/api\/v1\/movies\/.+ url response-body "success":\d+ response-body "success":1
+
+^https:\/\/yugedu\.com\/api\/v1\/users url script-response-body javdbs.js
+
+[mitm]
+hostname = yugedu.com
+
+JAV涓嬭浇鍦板潃锛氾紙娉ㄥ唽濉啓閭€璇风爜锛歴8rvea锛�
+https://jcapnred.com/?source=s8rvea
+
 */
-var obj = JSON.parse($response.body);
-    obj = {
- 
- "success": 1,
-  "action": null,
-  "message": null,
-  "data": {
-    "user": {
-      "id": 1791059,
-      "username": "六个核桃丫",
-      "email": "liangzai1043940928@gmail.com",
-      "is_vip": true,
-      "vip_expired_at": 2099-09-09T22:16:31.000+08:00,
-      "want_watch_count": 1,
-      "watched_count": 0,
-      "promote_users_count": 0,
-      "share_url": "https://jav7.app/?source=ayx7vw",
-      "promotion_code": "ayx7vw",
-      "promotion_days": 999,
-      "checkin_days": 0,
-      "last_checkin_at": null
+
+let obj = JSON.parse($response.body);
+obj = {
+  "success" : 1,
+  "message" : null,
+  "data" : {
+    "user" : {
+      "id" : 900000,
+      "promotion_days" : 999,
+      "checkin_days" : 0,
+      "want_watch_count" : 0,
+      "promotion_code" : "s8rvea",
+      "vip_expired_at" : "2099-09-09T22:16:31.000+08:00",
+      "username" : "ios榛戠鎶€",
+      "share_url" : "https://jav3.app/?source=s8rvea",
+      "last_checkin_at" : null,
+      "promote_users_count" : 999,
+      "email" : "ioshkj@163.com",
+      "is_vip" : true,
+      "watched_count" : 0
     },
-    "banner_type": "payment",
-    "following_tags": [
-      {
-        "id": 2442303,
-        "name": "字幕",
-        "value": "@:t:c::::",
-        "priority": 0
-      }
-    ],
-    "token": "eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTc5MTA1OSwidXNlcm5hbWUiOiJsaWFuZ3phaTEwNDM5NDA5MjgifQ.Gp1Cnq0U5g2GX8qxRW0Tbbn0BS743rSsuwB_jOgDd0k"
-  }
+    "banner_type" : "payment"
+  },
+  "action" : null
 }
-$done({body:JSON.stringify(obj)});
+;
+
+$done({body: JSON.stringify(obj)});
