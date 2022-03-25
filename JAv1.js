37ac4bdea8d0fafa0786b2abc2ece77af04f927a
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-26 01:55:32 +0800

    Update JAv1.js

--- JAv1.js
+++ JAv1.js
@@ -1,25 +1,29 @@
 /*
+寰俊鍏紬鍙凤細ios榛戠鎶€
+瀹樻柟缃戠珯锛歴7aa.cn
 
+鍦圶:
 
 [rewrite_local]
 
-#JAv1
-^https:\/\/yugedu\.com\/api\/v1\/movies\/.*\/play url script-request-header https://raw.githubusercontent.com/Shou00/Shou00/main/JAv1.js
+#JAv
+^https:\/\/yugedu\.com\/api\/v1\/movies\/(.+)\/play url script-request-header https://raw.githubusercontent.com/Shou00/Shou00/main/JAv1.js
 
-
+^https:\/\/yugedu\.com\/api\/v1\/movies\/.+ url response-body "success":\d+ response-body "success":1
 
 
 
 [mitm]
 hostname = yugedu.com
 
+JAV涓嬭浇鍦板潃锛氾紙娉ㄥ唽濉啓閭€璇风爜锛歴8rvea锛�
+https://jcapnred.com/?source=s8rvea
 
 */
 
 
-var hausd0rff = $request['headers'];
-hausd0rff['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MTQ0MjQ4MywidXNlcm5hbWUiOiJ5cWMwMDcifQ.EOCu_lF9TFxeZQ_2SfcwdyiU7cyBESkXiHfWBiP3k9M';
-hausd0rff['signature'] = '45aebf2239f533611b172a8ecff036a3';
-$done({
-    headers: hausd0rff
-});
+var modifiedHeaders = $request.headers;
+
+modifiedHeaders['authorization'] = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6OTgwNDk5LCJ1c2VybmFtZSI6Imlvc2hrajEifQ.aSlLcMcGxEtNrsxife9exe0BGs6C4bQHdiBUaYamEAM';
+
+$done({headers : modifiedHeaders});
