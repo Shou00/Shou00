1de7e7117dc305bb529d15a11fc0bccc9ab0446b
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-26 15:15:57 +0800

    Add files via upload

---
+++ ypjj.js
@@ -0,0 +1,18 @@
+
+/*
+ *
+ *
+        
+脚本功能：破解VIP
+
+*******************************
+[rewrite_local]
+# > ypjj解锁订阅
+^http:\/\/kongkongfufei\.pdf00\.com\/api\/client\/info.+ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/ypjj.js
+[mitm] 
+hostname = kongkongfufei.pdf00.com
+*
+*
+*/
+
+U2FsdGVkX19VwBCazKR3bMxvnQhbC//5YYCQmosIv+toN9XwBBQvVB3s6WNcazhZgyu9soQBj3LjV/ATzcgIRTFVRXkgu6gNvaleebDWC6IKA/S97rNWz0psEBOPSbwjvn/Kyd/2ozG/hTQtJzabVosVOGltv9TzJAJ5plsm+MKHsA8+XsLIBSv3LCPo/elKeFIK+eC6X9znR3G27IaYW0OuIjnibdz8UUQtkvQSvVkXOnmwhK9h9hwuycSiHYK/Yz197nkCh5BAnnHZS13kpTrSFApGvq29yI+Kb8NwjTJgSy+4/BMdMkVSQJEdORqfCA0sAQ+2x8YPWdQ+S7PcL8GqkHVbte5dQrRmUTsKCkDP6Yeo5B+cqXcu0GxXMIMgNue9udOoJgjzvABaZGCuSKxBMy9sUnVdr6F80HAP4w/Q4Z2RaEE1iI6bN8i7TGYdBy0FY32BZ4xuF/lGs99pZW9j11rHrBeVrSs5q+tedCdgDsXH0Kmrwr7llMeEkZYpocDYqKiN/U5A3cBekCBnum0NF9Y+ST/SX/Z+34CA59LlUqpIAFiOoC4wRXCrx7lJ7nqcECBbyH2l6TyX0uPrDgMf2hKksZlOgHhpvG3x7o5UhhukGGqJgKjEspUpMPQqyHl/GUhihGguTeCjc0dFuNx565nI/9O5h7LsAxl0Xd/vFUzTkZkXvgP30pBT4J2a1gvZff8M8Tu18qYwqCee2WS9uqfR5iZvg8AYezMgngzlQb0k48MlO62ebUvdcbaYUatq3wH1N3BwXvFVyMKR0/eCzbvKHtlHBh8Yd3d9NmAiyvdDJc9//LqE/rFCmkyl+x6bWeHxpS9pDM68xzLax05gufckZz3W34ymEiDInzMJbfgUVt9ts1RvXWt5UzxFeVXZbZj2SMWbFUas6/26BNH3x8JdnZjVHxLWgexPLH0VAeeUr1D9fWd7Z4dQLy+XxYF7qkmu1RLy2olqogcx/Pu9ZS/CY7Cuj4czp3d15VbH/7qbqo2PGOYrWEOHdyzyciAGW62nGw2zV2mDJ2ipwofpLy2EUn3kf6a/a93dtO66VTMU8mNf/62hxidiC/QA1kmL0YgG67t7ZUufBvf1k+agx7Xv3znXoYxOHfU01zFt/9o3jNfnyC8k9nXNXG5N6LQ6nTA5ZAOX/+izfTmOwmpSrps4CVoQDb9BWIMCIapM/Ba83Vsb/g/eiCaQNpK/7OFdVSFM0PeKWZ5tQHLMmq+HL4yBRIh3xs29ag0IgbBK196/Ic/LM7jNOQTrOSd1EXEskIS1W7sxljfu3XuD9z2VrljjJFyIR6Aizcd5lAyP8+gcDKKb1wGwrOA0aDgO
