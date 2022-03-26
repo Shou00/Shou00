4537c39c03fb5ff4e9f17cd577f516a1aff550ce
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-26 15:59:17 +0800

    Add files via upload

---
+++ ymjz.js
@@ -0,0 +1,22 @@
+          
+
+
+/*
+ *
+ *
+脚本功能：一木记账解锁VIP
+下载地址：苹果商店下载
+脚本作者：六个核桃🔥
+使用声明：⚠️此脚本仅供学习与交流，
+        请勿转载与贩卖！⚠️⚠️⚠️
+*******************************
+[rewrite_local]
+# > ymjz
+^https:\/\/yimuapp\.com(:8082)?\/bookkeeping\/user\/getUser\/ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/ymjz.js
+[mitm] 
+hostname = yimuapp.com:8082
+*
+*
+*/
+
+U2FsdGVkX189JzddNfWWObYrbg6pINL5IKjmUMPsDYTjyzUYGxBMLg+guAfBtHszJ3JaI2iC0BAEAnD+ZT6rwXFd2+JEuY19MytGnQAr7iEBLBBv0kLUvfNnWh1LwFqdNpwshPh1oTbDOCkqYSbLzfGLSKZ1bSf1mvm2X5IG07h8intEBajpbEVWFaMw/d8ytSxznNWMmkSpb2zbKiMLwN2qp0gqtyr9QX4udUorykl+f6w7sf+bMaafCP1KTqtnhUXYSEbESSST25Kgwyahb1ruOflzaLGr
