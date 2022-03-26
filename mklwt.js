071b4fbf12ea8b7ac577a0c9acc999c6e0589b59
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-26 21:35:23 +0800

    Add files via upload

---
+++ mklwt.js
@@ -0,0 +1,25 @@
+ 
+/*
+ *
+ *
+ 软件：马卡龙玩图      
+脚本功能：破解VIP
+
+*******************************
+[rewrite_local]
+# > mklwt
+https:\/\/app\.api\.versa-ai\.com\/user\/base\/getSelfUserBaseInfo.+ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/mklwt.js
+[mitm] 
+hostname = app.api.versa-ai.com
+
+*/
+
+ar obj = JSON.parse($response.body);
+
+if (obj.data && obj.data.result) {
+    obj.data.result.isVip = 1;
+    obj.data.result.userType = 3;
+}
+
+$done({
+            body: JSON.stringify(obj
