36461cf71be86757ba365ca10c01ec3e67fc2e62
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-27 17:57:16 +0800

    Update qyxj.js

--- qyxj.js
+++ qyxj.js
@@ -17,8 +17,6 @@ hostname = user-kaji-api.b612kaji.com
 *
 * 
 */
-
 let obj = JSON.parse($response.body);
-obj.data.flag = true; 
-obj.data.end_time = 3478758362;
+obj.data = {"flag":true,"start_time":0,"end_time":3999317826,"is_first_subscribe":true,"is_cancel_subscribe":false,"uid":"0","subscribe_uid":"did3052685194692735","subscribe_type":"un-auto","authkey":"2e2dcf2c47edb70f1b32bbcb707bdf37","product_id":"","subscribe_cycle":0,"cycle_unit":""}}
 $done({body: JSON.stringify(obj)});
