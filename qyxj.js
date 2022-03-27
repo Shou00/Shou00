156b0dcf6a0f584680e36e1efe6a07e3c131fc48
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-27 18:07:06 +0800

    Update qyxj.js

--- qyxj.js
+++ qyxj.js
@@ -17,6 +17,16 @@ hostname = user-kaji-api.b612kaji.com
 *
 * 
 */
-let obj = JSON.parse($response.body);
-obj.data = {"flag":true,"start_time":0,"end_time":3999317826,"is_first_subscribe":true,"is_cancel_subscribe":false,"uid":"0","subscribe_uid":"did3052685194692735","subscribe_type":"un-auto","authkey":"2e2dcf2c47edb70f1b32bbcb707bdf37","product_id":"","subscribe_cycle":0,"cycle_unit":""}}
-$done({body: JSON.stringify(obj)});
+var body = $response.body;
+ var url = $request.url;
+ var obj = JSON.parse(body);
+ const 1 = 'commerce-api.faceu.mobi/commerce/v1/subscription/user_info';
+ if (url.indexOf(1) != -1) {
+  obj.flag = true;
+  obj.end_time = 3742796226; 
+  
+}
+ $done({
+ body
+}
+);
