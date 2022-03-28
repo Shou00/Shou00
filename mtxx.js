3737118ae6e0d2bf59689c46230c97526a8291fb
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-28 22:22:39 +0800

    Add files via upload

---
+++ mtxx.js
@@ -0,0 +1,19 @@
+
+/*
+ *
+ *
+  软件：美图秀秀
+脚本功能：破解VIP（不显示VIP，功能正常用）
+*******************************
+[rewrite_local]
+# > 美图秀秀VIP解锁
+^https:\/\/api\.xiuxiu\.meitu\.com\/.*\/vip\/vip_show\.json.*$ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/mtxx.js
+
+[mitm] 
+hostname = *.xiuxiu.*.*
+*
+*
+*/
+
+
+U2FsdGVkX1+IFm6bRCyF5c3PeNNqZwIAcafq6UwpWpYhx9r+cy3Vy1LBcwy4/+OugN0zxVBeyYpvzhXnUDxgFhBph7FncWDxV4bralfFz/p9tC3rTxCrLTezFAuPgCO0xYn2sBQwj+gVz64hcE0UmV8/lANGN+cp8fSeUvBCMPf2K4wnCP8Vh66sD9Hxo1eDHDkJXYHJJZvpFIts1XZdvjJX4NVwYKBUjGNlqkndv4kVScXTXLRBHlCToGiWd70CMcShET4RAgRPPJxV6IKLdI9WOl/IGeFetojotJMNAC2j935aYh9QRVBBue1Jyvri9baIwidRTf3S9PbEWSgj9whNZFtdmPCnU6/94UbWzkQHfu4GyamDucXNWJR7/kEmB0ZGtgnpV7SLHOLPdyabRS0g2b6uq1hmxFDdZeehv12l+onSFIRRJoBMqv6+ibUDMLFdXhnxA4Tr3eKxyYl4xl8Al2hUytWn3sE5WAOGqsF7hhkgfGth6W0gyTUWVV4ss2g+5d5K+NqerBBD53DheL6ERuD5RXgqOhE5KmbYX4YD1So4uA8kA8BiOx/Qsb/PcBJHNpd+Ftr3NUfEKhxZvAqnRgSUzglioKMpzVu7Cx1iBlKrnVbpRLIyc6R9GUIRav3Z3NMbDcbf+1i9l6fJZC8ilgtTsY67UT4QIFeP+n7WrStUIEjyU7CkWk0CnkuBM1oW5CFHKcKRpDr3KQPaGMMH5Do6ujWCV+B67CaVYFDndnor24lOLEWKdpuvW3smKo9TOsHelPE=
