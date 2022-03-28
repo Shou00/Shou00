b7a7f47dbddec09f3610af636650564df4074455
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-29 00:21:48 +0800

    Update myxj.js

--- myxj.js
+++ myxj.js
@@ -6,7 +6,7 @@
 *******************************
 [rewrite_local]
 # > 美颜相机
-^https:\/\/api\.meiyan\.com\/vip\/user_info\.json url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/myxj.js
+^https:\/\/api\.meiyan\.com\/vip\/user_info\.json.*$ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/myxj.js
 
 [mitm] 
 hostname = api.meiyan.com
@@ -14,16 +14,16 @@ hostname = api.meiyan.com
 *
 */
 let obj = JSON.parse($response.body);
- obj.response.status = 1;
- obj.response.agreement_status = 2;
- obj.response.agreement_platform = 1;
- obj.response.expire_date = 2030-04-04;
- obj.response.expire_time = 2030-04-0400:00:00;
- obj.response.period_type = 3;
- obj.response.product_type = 2;
- obj.response.type = 3;
- obj.response.ad_vip_type = 4;
- obj.response.discount_status = 64;
+ obj.data.status = 1;
+ obj.data.agreement_status = 2;
+ obj.data.agreement_platform = 1;
+ obj.data.expire_date = 2030-04-04;
+ obj.data.expire_time = 2030-04-0400:00:00;
+ obj.data.period_type = 3;
+ obj.data.product_type = 2;
+ obj.data.type = 3;
+ obj.data.ad_vip_type = 4;
+ obj.data.discount_status = 64;
  $done({
  body: JSON.stringify(obj)
 }
