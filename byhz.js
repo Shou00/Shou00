6575ca9c7f95ff30626c625a3e62cca9a0c1edba
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-29 15:45:04 +0800

    Update byhz.js

--- byhz.js
+++ byhz.js
@@ -7,7 +7,7 @@
 *******************************
 [rewrite_local]
 # > 鲍鱼
-^https:\/\/[0-9.]*.\w{4}/api(/live/room/detail?|/video/report_item?|/video/detail//d+?|/video/related?|/live/plat/list?).*$ url script-request-header https://raw.githubusercontent.com/Shou00/Shou00/main/byhz.js
+^https:\/\/[0-9.]*.\w{4}/api(/live/room/detail?|/video/report_item?|/video/detail/|/video/related?|/live/plat/list?).*$ url script-request-header https://raw.githubusercontent.com/Shou00/Shou00/main/byhz.js
 
 [mitm] 
 hostname = *.*.*
