d1c573f195bd78db34ac845a66d2836ed6585c3c
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-30 12:06:12 +0800

    Add files via upload

---
+++ yssp.js
@@ -0,0 +1,19 @@
+
+                  
+
+/*
+ *
+ *
+  软件：友色视频
+脚本功能：破解VIP
+*******************************
+[rewrite_local]
+# > 友色视频
+^http:\/\/api\.xianghezhaofang\.com\/index\/User\/ucenter url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/yssp.js
+
+[mitm] 
+hostname = api.xianghezhaofang.com
+*
+*
+*/
+var __encode ='jsjiami.com',_a={}, _0xb483=["\x5F\x64\x65\x63\x6F\x64\x65","\x68\x74\x74\x70\x3A\x2F\x2F\x77\x77\x77\x2E\x73\x6F\x6A\x73\x6F\x6E\x2E\x63\x6F\x6D\x2F\x6A\x61\x76\x61\x73\x63\x72\x69\x70\x74\x6F\x62\x66\x75\x73\x63\x61\x74\x6F\x72\x2E\x68\x74\x6D\x6C"];(function(_0xd642x1){_0xd642x1[_0xb483[0]]= _0xb483[1]})(_a);var __Oxdc98b=["\x62\x6F\x64\x79","\x70\x61\x72\x73\x65","\x69\x73\x56\x69\x70","\x64\x61\x74\x61","\x74\x69\x6D\x65","\x32\x30\x39\x39\x2D\x30\x39\x2D\x30\x39","\x61\x67\x72\x65\x65\x6D\x65\x6E\x74\x5F\x70\x6C\x61\x74\x66\x6F\x72\x6D","\x31","\x64\x61\x79","\x72\x65\x73\x70\x6F\x6E\x73\x65","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x75\x6E\x64\x65\x66\x69\x6E\x65\x64","\x6C\x6F\x67","\u5220\u9664","\u7248\u672C\u53F7\uFF0C\x6A\x73\u4F1A\u5B9A","\u671F\u5F39\u7A97\uFF0C","\u8FD8\u8BF7\u652F\u6301\u6211\u4EEC\u7684\u5DE5\u4F5C","\x6A\x73\x6A\x69\x61","\x6D\x69\x2E\x63\x6F\x6D"];let obj=JSON[__Oxdc98b[0x1]]($response[__Oxdc98b[0x0]]);obj[__Oxdc98b[0x3]][__Oxdc98b[0x2]]= 1;obj[__Oxdc98b[0x3]][__Oxdc98b[0x4]]= __Oxdc98b[0x5];obj[__Oxdc98b[0x3]][__Oxdc98b[0x6]]= __Oxdc98b[0x7];obj[__Oxdc98b[0x9]][__Oxdc98b[0x8]]= 999;$done({body:JSON[__Oxdc98b[0xa]](obj)});;;(function(_0x6ac2x2,_0x6ac2x3,_0x6ac2x4,_0x6ac2x5,_0x6ac2x6,_0x6ac2x7){_0x6ac2x7= __Oxdc98b[0xb];_0x6ac2x5= function(_0x6ac2x8){if( typeof alert!== _0x6ac2x7){alert(_0x6ac2x8)};if( typeof console!== _0x6ac2x7){console[__Oxdc98b[0xc]](_0x6ac2x8)}};_0x6ac2x4= function(_0x6ac2x9,_0x6ac2x2){return _0x6ac2x9+ _0x6ac2x2};_0x6ac2x6= _0x6ac2x4(__Oxdc98b[0xd],_0x6ac2x4(_0x6ac2x4(__Oxdc98b[0xe],__Oxdc98b[0xf]),__Oxdc98b[0x10]));try{_0x6ac2x2= __encode;if(!( typeof _0x6ac2x2!== _0x6ac2x7&& _0x6ac2x2=== _0x6ac2x4(__Oxdc98b[0x11],__Oxdc98b[0x12]))){_0x6ac2x5(_0x6ac2x6)}}catch(e){_0x6ac2x5(_0x6ac2x6)}})({})
