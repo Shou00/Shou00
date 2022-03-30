3f3211474ce42950061aa6906423c961970c1847
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-03-30 12:59:14 +0800

    Add files via upload

---
+++ yssp.js
@@ -0,0 +1,24 @@
+
+/*
+ *
+ *
+  软件：友色视频
+链接：https://znrnmrjh.cn/index/Jump/index?agent_code=2957&invite_code=0
+*******************************
+[rewrite_local]
+# > 友色视频
+^http:\/\/api\.xianghezhaofang\.com\/index\/User\/ucenter url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/yssp.js
+
+[mitm] 
+hostname = api.xianghezhaofang.com
+*
+*
+*/
+let obj = JSON.parse($response.body);
+ obj.data.isVip = 1;
+ obj.data.time = "2099-09-09";
+ obj.data.day = "999"
+ $done({
+ body: JSON.stringify(obj)
+}
+);
