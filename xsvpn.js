0d311923bfac130ef293c6136b251f01306fb9f9
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-04-14 22:20:57 +0800

    Add files via upload

---
+++ xsvpn.js
@@ -0,0 +1,22 @@
+
+/*
+ *
+ *
+  软件：夏时加速器（2.4.7版本)
+脚本功能：破解vip
+*******************************
+[rewrite_local]
+# > 夏时vpn
+^https:\/\/.*\.com\/addressx2\/ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/xsvpn.js
+
+[mitm] 
+hostname = hotspot-unlimited.com
+*
+*
+*/
+let obj = JSON.parse($response.body);
+ obj.auto.vip = 0;
+ $done({
+ body: JSON.stringify(obj)
+}
+);
