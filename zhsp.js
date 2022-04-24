fe8a36f2315d9edaee86842e992af387bc99c3c8
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-04-24 15:19:08 +0800

    Add files via upload

---
+++ zhsp.js
@@ -0,0 +1,17 @@
+/*
+  汁乎&完整版url
+# > 
+
+^https:\/\/oiuohbvjdf\.pejyyah\.com\/api\/app\/media\/m3u8\/.+\/.+\/.+\/.+\/.+\/.+\.m3u8? url script-request-header https://raw.githubusercontent.com/Shou00/Shou00/main/zhsp.js
+
+QX MITM = kjsdhfkjb.qkjuyet.com
+
+
+
+*/
+
+var hausd0rff = $request['headers'];
+hausd0rff['authorization'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE2NTI2MTE3NjAsImlzc3VlciI6ImNvbS5idXR0ZXJmbHkiLCJzdWIiOiJhc2lnbiIsInVzZXJJZCI6NjkyMTA0MH0.-5F99qcn_6uycWfAa2Do16aqT7_i6WN8FbYfr_GGKp8';
+$done({
+    headers : hausd0rff
+});
