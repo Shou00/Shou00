b4c756d28ab0527f61e43a38a7c2f841b52dd83e
Shou00 <102242620+Shou00@users.noreply.github.com>
2022-06-17 07:00:37 +0800

    Add files via upload

---
+++ qzyp.js
@@ -0,0 +1,19 @@
+/******************************
+
+脚本功能：茄子云盘解锁会员
+
+*******************************
+[rewrite_local]
+# > 茄子云盘解锁会员
+^https?:\/\/121\.199\.197\.35:9293\/api\/ios\/getAdClick$ url script-response-body https://raw.githubusercontent.com/Shou00/Shou00/main/qzyp.js
+
+[mitm] 
+hostname = 121.199.197.35:9293
+
+*******************************/
+
+var objc = JSON.parse($response.body);
+objc.data = "48FC8FA298BF19484C11BA06445E76FB64F1A156E54E2F9B127CA4AF878ADD8C249CA31C34880FF42914AAD0B94CDFAF272B547E01BADCCDC080B7420C9DAAD0AB0E66887EFFF75E7DCE5BB5575F8CE32B94513345D23E166619BAE6F56A4DCB2570F25E018FFEE120FD809727F9123E187609FE63B75FAE8C686F3D1A64E5B0D4484353A673573E615841A1B9C7BA157EE2D24FEF78AB73DA57D9A70966DC27D6AD61567EF8ACCEFD9286E4E5478A36";
+$done({
+    body : JSON.stringify(objc)
+});
